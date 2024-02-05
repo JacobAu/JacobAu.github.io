f99cfa33efe8c6dc1b6054917c9b1908581e053b
Jacob Au <jacob.au@uwaterloo.ca>
2024-02-04 23:42:45 -0500

    surrpise fix

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,14 @@ const yes_button = document.querySelector(".yes");
 const no_button = document.querySelector(".no");
 const title_text = document.querySelector(".title-text");
 
-console.log(main_image);
 function onYesButtonClick() {
     main_image.src = "gallery/cat_yes.jpg";
     title_text.innerText = "YAAAAAAYYYYYYY"
-    console.log("yes clicked");
 }
 
 function onNoButtonClick() {
     main_image.src ="gallery/cat_no.jpg";
-    title_text.innerText = "GRRRRRRRRRRRRRRRRRRRRRRR"
-    console.log("no clicked");
+    title_text.innerText = "GRRRRRRRRRRRRRRRRRRRRRRR TRY AGAIN"
 }
 
 yes_button.onclick = onYesButtonClick;
